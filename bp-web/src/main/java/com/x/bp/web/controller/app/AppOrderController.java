d5e66b22d12484d8f90c2222902cfb60d1051404
zouzhe <611019475@qq.com>
2025-02-16 21:21:18 +0800

    订单相关

--- bp-web/src/main/java/com/x/bp/web/controller/app/AppOrderController.java
+++ bp-web/src/main/java/com/x/bp/web/controller/app/AppOrderController.java
@@ -1,8 +1,22 @@
 package com.x.bp.web.controller.app;
 
+import com.x.bp.common.model.ServiceResultTO;
+import com.x.bp.common.utils.ApiContextUtil;
+import com.x.bp.core.common.Result;
+import com.x.bp.core.dto.order.CreateOrderDTO;
+import com.x.bp.core.dto.order.CreateOrderReq;
+import com.x.bp.core.dto.user.UserLoginReq;
+import com.x.bp.core.service.order.OrderService;
+import com.x.bp.web.annotion.LoginNotRequired;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
 
 /**
  * @author zouzhe
@@ -13,4 +27,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/app/order")
 @Api(value = "订单")
 public class AppOrderController {
+
+    @Resource
+    private OrderService orderService;
+
+    @ApiOperation("创建订单")
+    @PostMapping("/createOrder")
+    public Result<CreateOrderDTO> createOrder(@RequestBody @Validated CreateOrderReq req) {
+        req.setUserId(ApiContextUtil.getUserId());
+        ServiceResultTO<CreateOrderDTO> createOrderResult = orderService.createOrder(req);
+        if (createOrderResult.isSuccess()) {
+            return Result.buildSuccess(createOrderResult.getData());
+        }
+        return Result.buildFail(createOrderResult.getMessage());
+    }
 }
